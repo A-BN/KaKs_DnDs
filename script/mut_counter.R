2e661f11af7f17ec97ba6817f3d9584e28c430d6
A-BN <antoine.bridier-nahmias@inserm.fr>
2022-08-31 16:54:17 +0200

    completed codon analysis

--- script/mut_counter.R
+++ script/mut_counter.R
@@ -17,21 +17,22 @@ mut_counter <- function(full_orf_aln,
   # creating empty output table
   mut_table <- genetic_code
   mut_table$AA <- NULL
-  mut_table$S <- 0
-  mut_table$NS <- 0
+  mut_table$s <- 0
+  mut_table$n <- 0
   
   # reading first sequence as a reference
+  message("Analyzing the first sequence as a reference.")
   to_skip <- 1 # in order to read lines one by one
   ref <- 
     read_codons(file = full_orf_aln, n = 1, skip = to_skip)
   # counting codons in REF
   ref_count <- codon_counter(ref, sn_table = SN_table)
-  print(ref_count)
   
   # reading the following sequences
-  while(to_skip < (n_lines)) {
+  message("Analyzing the following sequences")
+  while(to_skip < (n_lines - 1)) {
   to_skip <- to_skip + 2
-  print(to_skip)
+  # cat('lines to skip:', to_skip, '\n')
   curr_seq <- 
     read_codons(file = full_orf_aln, n = 1, skip = to_skip)
   # finding mismatches positions
@@ -42,15 +43,15 @@ mut_counter <- function(full_orf_aln,
     next()
   }
   for (i in 1:length(curr_mismatches)) {
-    print(ref[curr_mismatches[i]])
-    print(curr_seq[curr_mismatches[i]])
+    # print(ref[curr_mismatches[i]])
+    # print(curr_seq[curr_mismatches[i]])
     curr_mut <- syn_or_nonsyn(codon_ref = ref[curr_mismatches[i]], 
                   codon_mut = curr_seq[curr_mismatches[i]], 
                   gen_code = genetic_code)
-    mut_table$S[mut_table$codon == curr_mut$ref[1]] <- 
-      mut_table$S[mut_table$codon == curr_mut$ref[1]] + curr_mut$S[1]   
-    mut_table$NS[mut_table$codon == curr_mut$ref[1]] <- 
-      mut_table$NS[mut_table$codon == curr_mut$ref[1]] + curr_mut$NS[1]              
+    mut_table$s[mut_table$codon == curr_mut$ref[1]] <- 
+      mut_table$s[mut_table$codon == curr_mut$ref[1]] + curr_mut$s[1]   
+    mut_table$n[mut_table$codon == curr_mut$ref[1]] <- 
+      mut_table$n[mut_table$codon == curr_mut$ref[1]] + curr_mut$n[1]              
   }
 }
   mut_table <- merge(ref_count, mut_table, by = 'codon')
@@ -92,15 +93,14 @@ read_codons <- function(file, n = 1,skip) {
 }
 
 codon_counter <- function(ref, sn_table = SN_table) {
-  
   # count codon occurence
-  codon_count_table <- data.frame(codon=ref, count=0)
+  codon_count_table <- data.frame(codon=unique(ref), count=0)
   for (i in 1:length(ref)) {
     codon.i <- ref[i]
     codon_count_table$count[match(codon.i, codon_count_table$codon)] <- codon_count_table$count[match(codon.i, codon_count_table$codon)] + 1
   }
-  codon_count_table$s <- sn_table$S[which(sn_table$codon %in% codon_count_table$codon)]
-  codon_count_table$n <- sn_table$N[which(sn_table$codon %in% codon_count_table$codon)]
+  codon_count_table$S <- sn_table$S[match(codon_count_table$codon, sn_table$codon)]
+  codon_count_table$N <- sn_table$N[match(codon_count_table$codon, sn_table$codon)]
   return (codon_count_table)
 }
 
@@ -111,13 +111,14 @@ ONE OF THEM IS TOO LONG AND THE OTHER TOO SHORT,
 WHY G*D WHY HAVE YOU FORSAKEN US !')
   }
   mis <- which(seq_1 != seq_2)
+  # print(mis)
   return(mis)
 }
 
 syn_or_nonsyn <- 
   function(codon_ref, codon_mut, gen_code = genetic_code) {
     in_codons <- c(codon_ref, codon_mut)
-    result <- list(ref = codon_ref, mut = 'xxx', S = 0, NS = 0)
+    result <- list(ref = codon_ref, mut = 'xxx', s = 0, n = 0)
     
     codon_1_invalid <- grepl(pattern = '[^ATGC]', x = in_codons[1])
     codon_2_invalid <- grepl(pattern = '[^ATGC]', x = in_codons[2])
@@ -137,19 +138,20 @@ syn_or_nonsyn <-
     
     
     if (out_aa[1] == out_aa[2]) {
-      result <- list(ref = codon_ref, mut = codon_mut, S = 1, NS = 0)
+      result <- list(ref = codon_ref, mut = codon_mut, s = 1, n = 0)
     } else if (out_aa[1] != out_aa[2] ) {
-      result <- list(ref = codon_ref, mut = codon_mut, S = 0, NS = 1)
+      result <- list(ref = codon_ref, mut = codon_mut, s = 0, n = 1)
     }
   return(result)
 }
 
+#### test ----
 system.time({
 glou <-
   mut_counter("./data/core_gene_alignment_test.aln")
 })
 
 glou
-
-# glou_2 <- mut_counter('data/toy_dataset.aln')
+# rm(glou_2)
+# glou_2 <- mut_counter('data/rpoB_align_1l.aln')
 # glou_2
